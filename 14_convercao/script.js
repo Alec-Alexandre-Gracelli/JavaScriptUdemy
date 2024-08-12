fd916aea2cdab516ec0696046e98c17397c6bb8b
Alec-Alexandre-Gracelli <93772106+Alec-Alexandre-Gracelli@users.noreply.github.com>
2024-08-12 16:49:00 -0300

    lesson convencao

---
+++ 14_convercao/script.js
@@ -0,0 +1,9 @@
+// let 3nome = 'Teste'; Erro
+let nome3 = 'Teste1';
+let $nome = 'Teste2';
+let _nome = 'Teste3';
+// let @nome = 'Teste4'; Erro
+// let !nome = 'Teste5'; Erro
+let Nome = 'Teste6';
+let seuNome = 'Teste7';
+
