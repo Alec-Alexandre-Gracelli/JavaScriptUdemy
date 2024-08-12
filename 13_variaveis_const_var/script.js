4be042a50af21372ce619b798998c287ad8fff5e
Alec-Alexandre-Gracelli <93772106+Alec-Alexandre-Gracelli@users.noreply.github.com>
2024-08-12 16:42:47 -0300

    lesson variaveis

---
+++ 13_variaveis_const_var/script.js
@@ -0,0 +1,7 @@
+var nome  = "Alec";
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+ip = "adiajnd"; //const não pode ser alterado o seu valor, sendo que já foi atribuído um valor inicialmente.
